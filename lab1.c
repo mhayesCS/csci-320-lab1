79147c8b4be06056dccc08501a1fcdef59ab2fff
mhayesCS <mahayes21@students.desu.edu>
2023-10-09 03:16:30 +0000

    mysteryExploded function

--- lab1.c
+++ lab1.c
@@ -8,3 +8,13 @@ char* readString(char* fileName) {
     location[strcspn(location, "\n")] = '\0';
     return location;
 }
+
+char* mysteryExplode(const char* str){
+    int len = strlen(str);
+    char *exploded = (char*) malloc((len*(len+1)/2)+1);
+    memset(exploded, '\0', (len*(len+1)/2)+1);
+    for (int x = 1; x <=len; x++){
+        strncat(exploded, str, x);
+    }
+    return exploded;
+}
